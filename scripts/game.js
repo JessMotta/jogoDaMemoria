a35bbe439312595d714a300b7ac23b44b51b5fbe
JessMotta <jessicalimamotta@gmail.com>
2021-10-06 19:08:34 -0300

    game over and restart game

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,9 @@
 let game = {
+  //para travar a seleção de uma terceira carta e para comparar as cartas se são iguais
+  lockMode: false,
+  firstCard: null,
+  secondCard: null,
+
   techs: [
     "bootstrap",
     "css",
@@ -13,6 +18,48 @@ let game = {
   ],
 
   cards: null,
+
+  // verificar se a carta foi virada
+  setCard: function (id) {
+    let card = this.cards.filter((card) => card.id === id)[0];
+
+    if (card.flipped || this.lockMode) {
+      return false;
+    }
+    if (!this.firstCard) {
+      this.firstCard = card;
+      this.firstCard.flipped = true;
+      return true;
+    } else {
+      this.secondCard = card;
+      this.secondCard.flipped = true;
+      this.lockMode = true;
+      return true;
+    }
+  },
+
+  checkMatch: function () {
+    if (!this.firstCard || !this.secondCard) {
+      return false;
+    }
+    return this.firstCard.icon === this.secondCard.icon;
+  },
+
+  clearCards: function () {
+    this.firstCard = null;
+    this.secondCard = null;
+    this.lockMode = false;
+  },
+
+  unflipCards() {
+    this.firstCard.flipped = false;
+    this.secondCard.flipped = false;
+    this.clearCards();
+  },
+
+  checkGameOver() {
+    return this.cards.filter((card) => !card.flipped).length == 0;
+  },
 
   createCardsFromTechs: function () {
     this.cards = [];
@@ -29,8 +76,7 @@ let game = {
     this.cards = this.cards.flatMap((pair) => pair);
 
     this.shuffleCards();
-
-
+    return this.cards;
   },
 
   createPairFromTech: function (tech) {
